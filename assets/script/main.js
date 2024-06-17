c97a2fb64d25bcac5c4bde01c8191d2b34169df0
chiko12m <chiko.dct@gmail.com>
2024-06-18 02:37:22 +0900

    202406180237

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,11 +2,14 @@
 var hamburger = $('.header__hamburger-menu');
 
 $('.header__hamburger-button').on('click', function () {
-    console.log('Hamburger button clicked');
     hamburger.toggleClass('header__hamburger-menu--open');
 });
 
 $(window).on('resize', function () {
+    hamburger.removeClass('header__hamburger-menu--open');
+});
+
+$('.header__hamburger-menu-list-item').on('click', function () {
     hamburger.removeClass('header__hamburger-menu--open');
 });
 
