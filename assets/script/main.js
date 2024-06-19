965bf42af6e7dd0f9d31c3e925f873537d3a0db5
chiko12m <chiko.dct@gmail.com>
2024-06-20 00:15:15 +0900

    202406200015

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,15 +2,15 @@
 var hamburger = $('.header__hamburger-menu');
 
 $('.header__hamburger-button').on('click', function () {
-    hamburger.toggleClass('header__hamburger-menu-open');
+    hamburger.toggleClass('header__hamburger-menu--open');
 });
 
 $(window).on('resize', function () {
-    hamburger.removeClass('header__hamburger-menu-open');
+    hamburger.removeClass('header__hamburger-menu--open');
 });
 
 $('.header__hamburger-menu-list-item').on('click', function () {
-    hamburger.removeClass('header__hamburger-menu-open');
+    hamburger.removeClass('header__hamburger-menu--open');
 });
 
 // スマートタブ
